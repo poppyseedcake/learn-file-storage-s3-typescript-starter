73871599d573072347d888ef39f2398a848efb0d
poppyseedcake <w.makowiec@gmail.com>
2026-04-09 18:50:21 +0200

    boot thumbnail

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,6 +1,7 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
 import { getVideo, updateVideo } from "../db/videos";
+import { getInMemoryURL } from "./assets";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
@@ -45,9 +46,14 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
 
-  console.log("uploading thumbnail for video", videoId, "by user", userID);
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("Couldn't find video");
+  }
+  if (video.userID !== userID) {
+    throw new UserForbiddenError("Not authorized to update this video");
+  }
 
-  // TODO: implement the upload here
   const formData = await req.formData();
   const file = formData.get("thumbnail");
   if (!(file instanceof File)) {
@@ -55,27 +61,31 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   }
 
   const MAX_UPLOAD_SIZE = 10 << 20;
+
   if (file.size > MAX_UPLOAD_SIZE) {
-    throw new BadRequestError(`File size has to less than ${MAX_UPLOAD_SIZE}`);
+    throw new BadRequestError(
+      `Thumbnail file exceeds the maximum allowed size of 10MB`,
+    );
   }
 
-  const data: ArrayBuffer = await file.arrayBuffer();
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
+  const mediaType = file.type;
+  if (!mediaType) {
+    throw new BadRequestError("Missing Content-Type for thumbnail");
   }
 
-  if (video.userID != userID) {
-    throw new UserForbiddenError("Wrong UserId");
+  const fileData = await file.arrayBuffer();
+  if (!fileData) {
+    throw new Error("Error reading file data");
   }
 
-  videoThumbnails.set(videoId, { "data": data, "mediaType": file.type });
-  const thumbnailUrl: string = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
+  videoThumbnails.set(videoId, {
+    data: fileData,
+    mediaType,
+  });
 
-  video.thumbnailURL = thumbnailUrl;
+  const urlPath = getInMemoryURL(cfg, videoId);
+  video.thumbnailURL = urlPath;
   updateVideo(cfg.db, video);
-
 
   return respondWithJSON(200, video);
 }
