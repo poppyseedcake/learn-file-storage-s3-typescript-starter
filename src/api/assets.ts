663231523572a587aba5cb0133d9e779aac1fca1
poppyseedcake <w.makowiec@gmail.com>
2026-04-17 22:53:48 +0200

    dsadsa

--- src/api/assets.ts
+++ src/api/assets.ts
@@ -24,23 +24,3 @@ export function getAssetDiskPath(cfg: ApiConfig, assetPath: string) {
 export function getAssetURL(cfg: ApiConfig, assetPath: string) {
   return `http://localhost:${cfg.port}/assets/${assetPath}`;
 }
-
-export async function getVideoAspectRatio(filePath: string) {
-  //ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of json filePath
-  const proc = Bun.spawn(["ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "json", filePath], {
-    stdout: "pipe",
-    stderr: "pipe",
-  });
-  const stdoutText = await new Response(proc.stdout).text();
-  const stderrText = await new Response(proc.stderr).text();
-  const exited = await proc.exited;
-  if (exited !== 0) {
-    throw new Error(stderrText || `ffprobe exited with code ${exited}`);
-  }
-  const data = JSON.parse(stdoutText);
-  const { width, height } = data.streams[0];
-  const ratio = width / height;
-  if (ratio >= 1.7) return "landscape";
-  if (ratio <= 0.6) return "portrait";
-  return "other";
-}
